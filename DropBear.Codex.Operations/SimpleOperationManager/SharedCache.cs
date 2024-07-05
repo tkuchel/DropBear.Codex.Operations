f0ed14ff1a565108977de51671523653ca350090
DropBear <tkuchel@users.noreply.github.com>
2024-07-05 15:13:07 +1000

    Added underscore to cache field in SharedCache.cs

--- DropBear.Codex.Operations/SimpleOperationManager/SharedCache.cs
+++ DropBear.Codex.Operations/SimpleOperationManager/SharedCache.cs
@@ -2,22 +2,22 @@
 
 public class SharedCache
 {
-    private readonly Dictionary<string, object> cache = new(StringComparer.OrdinalIgnoreCase);
+    private readonly Dictionary<string, object> _cache = new(StringComparer.OrdinalIgnoreCase);
 
     public void Set<T>(string key, T value)
     {
-        if (value is not null) cache[key] = value;
+        if (value is not null) _cache[key] = value;
     }
 
     public T Get<T>(string key)
     {
-        if (cache.TryGetValue(key, out var value) && value is T typedValue) return typedValue;
+        if (_cache.TryGetValue(key, out var value) && value is T typedValue) return typedValue;
         throw new KeyNotFoundException($"Key '{key}' not found in cache or is not of type {typeof(T)}");
     }
 
     public bool TryGet<T>(string key, out T? value)
     {
-        if (cache.TryGetValue(key, out var objValue) && objValue is T typedValue)
+        if (_cache.TryGetValue(key, out var objValue) && objValue is T typedValue)
         {
             value = typedValue;
             return true;
